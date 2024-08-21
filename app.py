59d1ea9bd9ce3f4721c9f3cfb2b5d8dbbd04fca4
henrique_machado04 <hm982788@gmail.com>
2024-08-20 21:29:25 -0300

    20/08

--- app.py
+++ app.py
@@ -28,13 +28,42 @@ def dicionarios():
     return render_template('dicionarios.html', usuario=usuario_info)
 
 @app.route('/listas_dicionarios')
-def lista_dicionarios():
-    usuarios = {
-        ["nome": "Henrique", "idade": 20, "cidade": "Jaraguá do Sul"],
-        ["nome": "Gabrieli", "idade": 18, "cidade": "Guaramirim"]
-        ["nome": "Paula", "idade": 9, "cidade": "Jaraguá do Sul"]
+def listas_dicionarios():
+    usuarios = [
+        {"nome": "Henrique", "idade": 20, "cidade": "Jaraguá do Sul"},
+        {"nome": "Gabrieli", "idade": 18, "cidade": "Guaramirim"},
+        {"nome": "Paula", "idade": 9, "cidade": "Jaraguá do Sul"},
+    ]
+    return render_template('listas_dicionarios.html', usuarios=usuarios)
+
+@app.route('/controle_fluxo_condicionais')
+def controle_fluxo_condicionais():
+    usuario = {
+        "nome": "Henrique",
+        "idade": 20,
+        "cidade": "Jaraguá do Sul",
+        "premium": True,
     }
-    return render_template('listas_dicionarios', usuarios = usuarios)
+    return render_template('controle_fluxo_condicionais.html', usuario=usuario)
+
+@app.route('/filtros_jinja')
+def filtros_jinja():
+    usuario = {
+        "nome": "Henrique",
+        "idade": 20,
+        "cidade": "Jaraguá do Sul",
+    }
+    mensagem = "Bem-vindo ao nosso site!"
+    return render_template('filtros_jinja.html', usuario=usuario, mensagem=mensagem)
+
+@app.route('/listas_postagem')
+def listas_postagem():
+    postagens = [
+        {"Id": 1, "Título": "A proura do Amor", "Conteúdo": "Palavras de amor."},
+        {"Id": 2, "Título": "Encontrando o Amor", "Conteúdo": "Como encontrar o seu amor."},
+        {"Id": 3, "Título": "Perdendo o Amor", "Conteúdo": "O que fazer quando perder o amor."},
+    ]
+    return render_template('listas_postagem.html', postagens=postagens)
 
 if __name__ == '__main__':
     app.run(debug=True)
